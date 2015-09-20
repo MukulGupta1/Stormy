55658b953b7e7eb0befa021ca5a61db714231a9b
Mukul Gupta <mukul@Mukuls-MacBook-Pro.local>
2015-09-20 22:44:08 +0530

    Making app location aware

---
+++ app/src/main/java/com/mukulgupta/stormy/CurrentLocation.java
@@ -0,0 +1,25 @@
+package com.mukulgupta.stormy;
+
+/**
+ * Created by mukul on 20/09/15.
+ */
+public class CurrentLocation {
+    private String mCity;
+    private String mCountry;
+
+    public String getCity() {
+        return mCity;
+    }
+
+    public void setCity(String city) {
+        mCity = city;
+    }
+
+    public String getCountry() {
+        return mCountry;
+    }
+
+    public void setCountry(String country) {
+        mCountry = country;
+    }
+}
